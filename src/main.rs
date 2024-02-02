83a0541fa00b82feb37937c1de08095e74a29e14
Kristian Gaylord <krlgaylord@gmail.com>
2024-02-02 18:55:28 -0500

    remove slot

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use cryptoki::context::{CInitializeArgs, Pkcs11};
-use cryptoki::slot::Slot;
 use once_cell::sync::Lazy;
 use tokio::sync::Mutex;
 
@@ -7,18 +6,14 @@ static HELLO: Lazy<Mutex<TestHsm>> = Lazy::new(|| Mutex::new(TestHsm::new()));
 
 pub struct TestHsm {
     pkcs11: Pkcs11,
-    slot: Slot,
 }
 
 impl TestHsm {
     pub fn new() -> Self {
         let pkcs11 = Pkcs11::new(env!("PKCS11_MODULE")).unwrap();
         pkcs11.initialize(CInitializeArgs::OsThreads).unwrap();
-        let slots = pkcs11.get_slots_with_initialized_token().unwrap();
-        let slot = slots.into_iter().find(|&s| pkcs11.get_token_info(s).unwrap().label().trim() == "TokenZero").unwrap();
         Self {
             pkcs11,
-            slot,
         }
     }
 }
